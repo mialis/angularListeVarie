b188998c002b02325873237fdf895458a53e21ef
alice <cecchetti.1800074@gmail.com>
2018-12-04 17:22:15 +0100

    added cards recipes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  selector: "app-recipe-item",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.scss"]
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() currentRecipe: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.scss"]
 })
 export class RecipeListComponent implements OnInit {
+  ricetta: Recipe = new Recipe(
+    "Patate riso e cozze",
+    "Lorem ipsum bla bla ",
+    "https://ips.plug.it/cips/buonissimo.org/cms/2012/04/tiella-di-riso-patate-e-cozze.jpg"
+  );
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      "Patate riso e cozze",
+      "Lorem ipsum bla bla ",
+      "https://ips.plug.it/cips/buonissimo.org/cms/2012/04/tiella-di-riso-patate-e-cozze.jpg"
+    ),
+    new Recipe(
+      "Carbonara",
+      "Lorem ipsum bla bla ",
+      "https://www.informacibo.it/wp-content/uploads/2018/04/carbonara.jpg"
+    ),
+    new Recipe(
+      "Amatriciana",
+      "Lorem ipsum bla bla ",
+      "http://www.sedanoallegro.it/sites/default/files/ricette/2017-10/amatriciana.jpeg"
+    )
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
